123060e6ac10b8a604cc8044dafc1d40530db5e7
Raul Dionisio <rauldionisiosh@gmail.com>
2021-10-30 13:35:15 -0300

    inserindo informações sobre o vídeo principal

--- js/video/setup.js
+++ js/video/setup.js
@@ -9,3 +9,16 @@ $(document).ready(function() {
 		fixedContentPos: false
 	});
 });
+
+$(function () {
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		modal: true
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
